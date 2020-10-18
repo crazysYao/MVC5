78c38d056192a59e0cd808e483739ad41905b549
YAO <a39795979@gmail.com>
2020-10-18 16:02:42 +0800

    Revert "08 替 Person 模型加上必填(Required)與範圍驗證(Range)"
    This reverts commit 3e1b7e6f905e35ed34b2ab08b64510b57780d3d3.

--- Models/Person.cs
+++ Models/Person.cs
@@ -9,10 +9,10 @@ namespace MVC5.Models
     public class Person
     {
         public int id { get; set; }
-        [Required(ErrorMessageResourceName = "Person_Name_Required", ErrorMessageResourceType = typeof(Resource1))]
+        [Required(ErrorMessage ="請輸入姓名")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "請輸入年紀")]
-        [Range(18, 99, ErrorMessage = "請輸入年紀必須介於 {1} - {2} 歲之間")]
+        [Required(ErrorMessage ="請輸入年紀")]
+        [Range(18,99,ErrorMessage ="請輸入年紀18到99")]
         public int Age { get; set; }
     }
 }
