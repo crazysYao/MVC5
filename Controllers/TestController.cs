a953e1d9bbb22d4a8b56df3fbdd30763de8e60cb
YAO <a39795979@gmail.com>
2020-10-18 11:06:18 +0800

    加入 Person 模型類別與 TestController & Index View

---
+++ Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using MVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var data = new List<Person>
+            {
+                new Person() {id=1, Name = "Will", Age =18 },
+                new Person() {id=2, Name = "Apple", Age =18 },
+                new Person() {id=3, Name = "banana", Age =18 },
+                new Person() {id=4, Name = "haha", Age =18 },
+            };
+            return View(data);
+        }
+    }
+}
