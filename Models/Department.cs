6a872c1d621ab48a2d7a866e54db67323f98bc5b
YAO <a39795979@gmail.com>
2020-10-24 13:09:56 +0800

    新增 DepartmentsController 的 Create Delete Edit Index 動作

--- Models/Department.cs
+++ Models/Department.cs
@@ -11,7 +11,8 @@ namespace MVC5.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Department
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,10 +20,15 @@ namespace MVC5.Models
         {
             this.Course = new HashSet<Course>();
         }
-    
+
         public int DepartmentID { get; set; }
         public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         public decimal Budget { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public System.DateTime StartDate { get; set; }
         public Nullable<int> InstructorID { get; set; }
         public byte[] RowVersion { get; set; }
