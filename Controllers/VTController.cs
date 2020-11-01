66a9e4f40be84a754ef1e5a6e6647d0c814d6d25
YAO <a39795979@gmail.com>
2020-11-01 11:03:02 +0800

    Bootstrap_Metro_Dashboard-

---
+++ Controllers/VTController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class VTController : Controller
+    {
+        // GET: VT
+        public ActionResult Index()
+        {
+            ViewBag.data = new int[] { 1, 2, 3, 4, 5 };
+            return View();
+        }
+    }
+}
