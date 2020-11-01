2ee070c0f38281063b58c8f7ecc7b1fc231bde36
YAO <a39795979@gmail.com>
2020-11-01 11:20:34 +0800

    Add Bootstrap_Metro_Dashboard

---
+++ Controllers/MetroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class MetroController : Controller
+    {
+        // GET: Metro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
